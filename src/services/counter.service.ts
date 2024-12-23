f53f764cf8642ff4e12942723d9d5f7afed046b6
Andrew Clark <cap@mail.com>
2024-12-23 03:07:24 +0100

    Corrected group name in data auth
    Tidied counter service a little

--- src/services/counter.service.ts
+++ src/services/counter.service.ts
@@ -41,12 +41,28 @@ class CounterService {
     return response.data;
   }
 
-  async findCounter(counterName: string): Promise<Counter | null> {
-    const response = await client.models.Counter.get({
-      name: counterName,
-    });
 
-    return response.data;
+  async exists(externalId: string, type: string): Promise<boolean> {
+    try {
+      const importedObject = await client.models.ImportedObject.get({ externalId, type });
+      return !!importedObject; // Return true if the Todo exists, false otherwise
+    } catch (error) {
+      console.error('Error checking Todo existence:', error);
+      return false; // Return false in case of any other errors
+    }
+  }
+
+  async findCounter(counterName: string): Promise<Counter | null> {
+    try {
+      const counter = await client.models.Counter.get({ name: counterName });
+      if (!counter) {
+        return null; // Return null if the Todo is not found
+      }
+      return counter.data;
+    } catch (error) {
+      throw this.serviceError(error, 'findCounter');
+    }
+
   }
 
 
@@ -65,22 +81,27 @@ class CounterService {
 
   async initCounter(counterName: string): Promise<Counter> {
     // Check if counter already exists
-    const existingCounter = await this.findCounter(counterName);
-    let response;
-    if (existingCounter) {
-      // Update existing counter
-      response = await client.models.Counter.update({
-        ...existingCounter,
-        val: 0,
-      });
-    } else {
-      // Create new counter
-      response = await client.models.Counter.create({
-        name: counterName,
-        val: 0,
-      });
+    try {
+
+      const existingCounter = await this.findCounter(counterName);
+      let response;
+      if (existingCounter) {
+        // Update existing counter
+        response = await client.models.Counter.update({
+          ...existingCounter,
+          val: 0,
+        });
+      } else {
+        // Create new counter
+        response = await client.models.Counter.create({
+          name: counterName,
+          val: 0,
+        });
+      }
+      return response.data!;
+    } catch (error) {
+      throw this.serviceError(error, 'initCounter');
     }
-    return response.data!;
   }
 
 }
