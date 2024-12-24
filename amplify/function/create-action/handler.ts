2446388b4383ee78e4b4be9a87f6568f3914f3b0
Andrew Clark <cap@mail.com>
2024-12-24 17:29:10 +0100

    create action working now logs all account activities

--- amplify/function/create-action/handler.ts
+++ amplify/function/create-action/handler.ts
@@ -1,5 +1,18 @@
 import type { DynamoDBStreamHandler } from "aws-lambda";
+import { type Schema } from "../../data/resource";
+import { generateClient } from "aws-amplify/data";
 import { Logger } from "@aws-lambda-powertools/logger";
+// import { Amplify } from "aws-amplify";
+// import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
+// import { env } from "$amplify/env/create-action-function";
+
+// const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
+//     env
+// );
+
+// Amplify.configure(resourceConfig, libraryOptions);
+
+const client = generateClient<Schema>();
 
 const logger = new Logger({
     logLevel: "INFO",
@@ -8,13 +21,28 @@ const logger = new Logger({
 
 export const handler: DynamoDBStreamHandler = async (event) => {
     for (const record of event.Records) {
-        logger.info(`Processing record: ${record.eventID}`);
-        logger.info(`Event Type: ${record.eventName}`);
-        logger.info(`Event: ${JSON.stringify(record)}`);
+        try {
+            logger.info(`Processing record: ${record.eventID}`);
+            logger.info(`Event Type: ${record.eventName}`);
+            logger.info(`Event: ${JSON.stringify(record)}`);
 
-        if (record.eventName === "INSERT") {
-            // business logic to process new records
-            logger.info(`New Image: ${JSON.stringify(record.dynamodb?.NewImage)}`);
+            if (record.eventName === "INSERT") {
+                // business logic to process new records
+                logger.info(`New Image: ${JSON.stringify(record.dynamodb?.NewImage)}`);
+                const { errors } = await client.models.Action.create({
+                    description: "Created a Account",
+                    userId: "defaultUsername",
+                    // Initial settings
+                });
+                if (errors) {
+                    console.error(`Errors in creating action:`, errors);
+                }
+
+
+
+            }
+        } catch (error) {
+            console.error(`Error creating profile:`, error);
         }
     }
     logger.info(`Successfully processed ${event.Records.length} records.`);
