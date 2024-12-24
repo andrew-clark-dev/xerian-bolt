6add4fec2d8080cd5e77382f70ad9fceee696c94
Andrew Clark <cap@mail.com>
2024-12-24 19:10:04 +0100

    Corrects theme use

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Authenticator } from '@aws-amplify/ui-react';
+import { ThemeProvider } from './context/ThemeContext';
 import { Layout } from './components/Layout';
 import { Dashboard } from './pages/Dashboard';
 import { Profile } from './pages/Profile';
@@ -20,22 +21,24 @@ export default function App() {
       signUpAttributes={['nickname']}
     >
       {({ signOut }) => (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout onSignOut={signOut} />}>
-              <Route index element={<Dashboard />} />
-              <Route path="accounts" element={<Accounts />} />
-              <Route path="accounts/new" element={<NewAccount />} />
-              <Route path="accounts/:number" element={<UpdateAccount />} />
-              <Route path="users" element={<Users />} />
-              <Route path="users/new" element={<NewUser />} />
-              <Route path="users/:email" element={<UpdateUserProfile />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="maintenance" element={<Maintenance />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout onSignOut={signOut} />}>
+                <Route index element={<Dashboard />} />
+                <Route path="accounts" element={<Accounts />} />
+                <Route path="accounts/new" element={<NewAccount />} />
+                <Route path="accounts/:number" element={<UpdateAccount />} />
+                <Route path="users" element={<Users />} />
+                <Route path="users/new" element={<NewUser />} />
+                <Route path="users/:email" element={<UpdateUserProfile />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="maintenance" element={<Maintenance />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
       )}
     </Authenticator>
   );
