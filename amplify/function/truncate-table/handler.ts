c4d198216c94a072f81d007d08dfa5c90f1ae48b
Andrew Clark <cap@mail.com>
2025-02-21 00:14:21 +0100

    Small update to logging

--- amplify/function/truncate-table/handler.ts
+++ amplify/function/truncate-table/handler.ts
@@ -6,7 +6,7 @@ import { truncateTable } from "../../lib/services/table.sevice";
 
 export const handler = async (event: { table_env_var: string, index: string[] }) => {
 
-    logger.info(`Truncating table with`, event);
+    logger.info(`Truncating table with event`, event);
     await truncateTable(event.table_env_var, event.index);
     return `Table truncated successfully`;
 
